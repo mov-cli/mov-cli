d69656059052d2f85c29203acc20b265ba4eabf4
Ananas <83706294+R3tr0Ananas@users.noreply.github.com>
2022-08-11 04:07:42 +0200

    Added Automatic Check for Updates

---
+++ mov_cli/utils/update.py
@@ -0,0 +1,31 @@
+from ..__version__ import __core__
+import requests
+import sys
+import subprocess
+
+
+class update:
+    def __init__(self):
+        pass
+    @staticmethod
+    def checkupdate():
+        unformated = requests.get("https://raw.githubusercontent.com/mov-cli/mov-cli/v3/__version__.py").text
+        newversion = unformated.split("__core__ = ")[1]
+        # strip all dots
+        newversion = newversion.replace(".", "")
+        localversion = __core__.replace(".", "")
+        if newversion == localversion:
+            print("No update available")
+        else:
+            print("[!] New version available! | ", unformated)
+            print("[!] You can download it from https://github.com/mov-cli/mov-cli")
+            choice = input("[!] Do you want to update? [y/n]: ").lower()
+            if choice == "y":
+                print("[!] Updating...")
+                subprocess.Popen("pip install git+https://github.com/mov-cli/mov-cli")
+                sys.exit(1)
+            else:
+                return
+
+
+    
