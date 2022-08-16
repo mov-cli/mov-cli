e71dc477dc74c41bde5f5f861c64f8e1ad7587bb
Ananas <83706294+R3tr0Ananas@users.noreply.github.com>
2022-08-16 15:01:17 +0200

    Push Discord Rich Presence

--- mov_cli/utils/config.py
+++ mov_cli/utils/config.py
@@ -76,3 +76,37 @@ class config:
         with open(file, "a") as f:
             f.truncate(0)
             f.write(f"download: {download}")
+
+    
+
+    @staticmethod
+    def presenceexists():
+        file = f"{config.winorlinux()}/mov_cli/.presence"
+        if os.path.exists(file):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def getpresence():
+        if config.presenceexists() is True:
+            file = f"{config.winorlinux()}/mov_cli/.presence"
+            with open(file, "r") as f:
+                presence = f.read().split("presence: ")[1]
+                if presence == "true":
+                    return True
+                else:
+                    return False
+        else:
+            return False
+
+    @staticmethod
+    def setpresence(disoren):
+        if os.path.exists(f"{config.winorlinux()}/mov_cli"):
+            pass
+        else:
+            os.mkdir(f"{config.winorlinux()}/mov_cli")
+        file = f"{config.winorlinux()}/mov_cli/.presence"
+        with open(file, "a") as f:
+            f.truncate(0)
+            f.write(f"presence: {disoren}")
---
+++ mov_cli/utils/presence.py
@@ -0,0 +1,19 @@
+from pypresence import Presence
+import re
+import time
+import os
+from .config import config
+
+dc = Presence("1008936657241767936")
+dc.connect()
+
+def update_presence(name):
+    if config.getpresence() is True:
+        name = re.sub(r"-+", " ", name)
+        name = re.sub(r"_+", " ", name)
+        dc.update(state=f"Watching {name}", large_image="logo", large_text="mov-cli", buttons=[{"label": "mov-cli", "url": "https://mov-cli.github.io/"}], start=time.time())
+    else:
+        pass
+def clear_presence():
+    dc.clear()
+
