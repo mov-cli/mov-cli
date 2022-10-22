5b1b72b9cef96254689985501157500023943948
Poseidon444 <87462580+Poseidon444@users.noreply.github.com>
2022-10-22 16:13:01 +0530

    More ignore

---
+++ mov_cli/utils/keep_alive.py
@@ -0,0 +1,36 @@
+import time
+# import multiprocessing as mp
+# import threading
+
+from .httpclient import HttpClient
+
+
+class KP:
+    def __init__(self, website: str, time: int = 120):
+        self.client = HttpClient()
+        self.time = time
+        self.website = website
+        # self.cr_th()
+
+    def ping(self, website: str = None):
+        if not website:
+            website = self.website
+        while True:
+            x = self.client.get(website)
+            print(x)
+            time.sleep(self.time)
+
+    # def cr_th(self):
+    #    t1 = threading.Thread(target=asyncio.run(self.ping()))
+    #    t1.start()
+
+# def main():
+#    mp.set_start_method('spawn')
+#    x = KP("https://www.google.com", 2)
+#    m1 = mp.Process(asyncio.run(x.ping()))
+#    m1.start()
+#    m1.join()
+#    print("hello")
+#
+
+# main()
