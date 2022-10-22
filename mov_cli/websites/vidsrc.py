87600028a3bbd982a8fd66b948a3c9274278e816
Poseidon444 <87462580+Poseidon444@users.noreply.github.com>
2022-10-22 16:17:28 +0530

    fix
    :wq
    :;::;:

--- mov_cli/websites/vidsrc.py
+++ mov_cli/websites/vidsrc.py
@@ -1,38 +1,45 @@
 from bs4 import BeautifulSoup as BS
 import os
+import threading
 import sys
 from ..utils.scraper import WebScraper
+from ..utils.keep_alive import KP
+
 sys.path.append("..")
 import httpx
+
 
 class Vidsrc(WebScraper):
     def __init__(self, base_url):
         super().__init__(base_url)
         self.base_url = base_url
         self.stream = "https://vidsrc.stream/pro/"
-        self.streamh = {"Referer": "https://source.vidsrc.me/", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
-        self.headers = {"Referer": "https://v2.vidsrc.me", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
-        self.finalheaders = {"Referer": "https://vidsrc.stream/", "Origin": "https://vidsrc.stream", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
-        
+        self.streamh = {"Referer": "https://source.vidsrc.me/",
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
+        self.headers = {"Referer": "https://v2.vidsrc.me",
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
+        self.finalheaders = {"Referer": "https://vidsrc.stream/", "Origin": "https://vidsrc.stream",
+                             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
+        self.keep_alive = KP(self.stream)
 
     def search(self, q: str = None):
-        q =( 
+        q = (
             input(self.blue("[!] Please Enter the name of the Movie: "))
             if q is None
             else q
         )
         return q
 
-    def results (self, html: str) -> list:
+    def results(self, html: str) -> list:
         data = httpx.get(f"https://v2.sg.media-imdb.com/suggestion/{html[0]}/{html}.json", headers=self.headers).json()
         ids = [data["d"][i]["id"] for i in range(len(data["d"]))]
         title = [data["d"][i]["l"] for i in range(len(data["d"]))]
         urls = ["/embed/" + data["d"][i]["id"] for i in range(len(data["d"]))]
         mov_or_tv = ["TV" if data["d"][i]["qid"].__contains__("tvSeries") else "MOVIE"
-            for i in range(len(data["d"]))
-        ]
+                     for i in range(len(data["d"]))
+                     ]
         return [list(sublist) for sublist in zip(title, urls, ids, mov_or_tv)]
-    
+
     def get_playeriframe(self, embed):
         url = self.base_url + embed
         re = httpx.get(url, headers=self.headers)
@@ -50,7 +57,7 @@ class Vidsrc(WebScraper):
         season = input(
             self.lmagenta(
                 f"Please input the season number(total seasons:{seasons}): "
-        ))
+            ))
         z = self.client.get(f"https://www.imdb.com/title/{imdb}/episodes?season={season}")
         episodes = soup.findAll("div", {"class": "list_item"})
         episode = input(
@@ -59,7 +66,7 @@ class Vidsrc(WebScraper):
             )
         )
         return season, episode
-    
+
     def cdn_url(self, iframe):
         stream = self.stream + iframe
         re = httpx.get(stream, headers=self.streamh).text
@@ -75,6 +82,8 @@ class Vidsrc(WebScraper):
         print(actlink)
         url = script.split("/")
         url = "https://" + url[19] + "/" + url[20] + "/" + url[21] + "/index-v1-a1.m3u8"
+        t1 = threading.Thread(target=self.keep_alive.ping, args=(actlink,))
+        t1.start()
         return url, actlink
 
     def enabler(self, path):
@@ -88,11 +97,11 @@ class Vidsrc(WebScraper):
         url, enable = self.cdn_url(iframe)
         self.enabler(enable)
         print(url)
-        #History.addhistory(self.userinput, state, "", season)
+        # History.addhistory(self.userinput, state, "", season)
         if state == "d":
             self.dl(url, name)
             return
-        #update_presence(t[self.title], season)
+        # update_presence(t[self.title], season)
         self.play(url, name)
 
     def MOV_PandDP(self, m: list, state: str = "d" or "p" or "sd"):
