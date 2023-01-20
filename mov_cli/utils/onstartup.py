8cf7bfa775a60557d07dcf275a942220e42e4592
Ananas <83706294+R3tr0Ananas@users.noreply.github.com>
2023-01-20 04:50:49 +0100

    Update onstartup.py

--- mov_cli/utils/onstartup.py
+++ mov_cli/utils/onstartup.py
@@ -17,30 +17,10 @@ class startup:
             return f"/home/{getpass.getuser()}"
         elif plt == "Darwin":
             return f"/Users/{getpass.getuser()}"
-    
-    @staticmethod
-    def getghkey():
-        try:
-            with open(f"{startup.winorlinux()}/gh.txt", "r") as f:
-                return f.read()
-        except: 
-            return None 
-
+        
     @staticmethod
     def getkey():
-        ghkey = startup.getghkey()
-        if ghkey is None:
-            dokicloud = httpx.get("https://api.github.com/repos/consumet/rapidclown/commits/dokicloud").json()
-            rabbitstream = httpx.get("https://api.github.com/repos/consumet/rapidclown/commits/rabbitstream").json()
-        else:
-            dokicloud = httpx.get("https://api.github.com/repos/consumet/rapidclown/commits/dokicloud", headers={"Authorization": f"Bearer {ghkey}"}).json()
-            rabbitstream = httpx.get("https://api.github.com/repos/consumet/rapidclown/commits/rabbitstream", headers={"Authorization": f"Bearer {ghkey}"}).json()
-        dokidate = dokicloud["commit"]["author"]["date"]
-        rabbitdate = rabbitstream["commit"]["author"]["date"]
-        if dokidate > rabbitdate:
-            decryptkey = "https://raw.githubusercontent.com/consumet/rapidclown/dokicloud/key.txt"
-        else:
-            decryptkey = "https://raw.githubusercontent.com/consumet/rapidclown/rabbitstream/key.txt"
-        u = httpx.get(decryptkey).text
+        key = httpx.get("https://raw.githubusercontent.com/mov-cli/movkey/main/key.txt").text
+        u = httpx.get(key).text
         with open(f"{startup.winorlinux()}/movclikey.txt", "w") as f:
             f.write(u)
