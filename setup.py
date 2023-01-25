0ab081f454a75bc6c51373958c621c06aa97648c
Ananas <83706294+R3tr0Ananas@users.noreply.github.com>
2023-01-25 16:41:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="mov-cli",
-    version="0.7.1",
+    version="0.7.2",
     author="pain@poseidon444",
     author_email="painedposeidon444@gmail.com",
     maintainer="ananas@r3tr0ananas",
