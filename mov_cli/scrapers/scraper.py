c99dc913eb9f88e9a2453a285f4b516e6b8004f1
THEGOLDENPRO <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-09-06 17:23:11 +0100

    fixed white space and renamed a few things

--- mov_cli/scrapers/scraper.py
+++ mov_cli/scrapers/scraper.py
@@ -1,6 +1,6 @@
 import httpx
 
-DEFAULT: dict = {
+DEFAULT_HEADERS: dict = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0",
     "Accept-Language": "en-US,en;q=0.5",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
@@ -11,9 +11,9 @@ __all__ = ("Scraper",)
 class Scraper():
     def __init__(self, base_url) -> None:
         """Anything HTTP/HTTPS related for mov-cli"""   
-        self.http = httpx.Client(timeout=15.0, headers=DEFAULT, cookies=None)
+        self.http = httpx.Client(timeout=15.0, headers=DEFAULT_HEADERS, cookies=None)
         self.base_url = base_url
-    
+
     def get(self, url: str, redirect: bool = False) -> httpx.Response:
         """Makes a GET request and returns httpx.Response"""
         self.http.headers["Referer"] = url
@@ -37,12 +37,12 @@ class Scraper():
         Not recommended
         """
         self.http.headers = header
-    
+
     def add_header_elem(self, header_elem: dict) -> None:
         """Add header elements to default header."""
         for elem in header_elem:
             self.http.headers[elem[0]] = elem[1]
-    
+
     def set_cookies(self, cookies: dict) -> None:
         """Sets cookies."""
         self.http.cookies = cookies
@@ -50,16 +50,15 @@ class Scraper():
     def search(self, query: str) -> httpx.Response:
         """Search anything. Returns httpx.Response"""
         ...
-    
+
     def results(self, response: httpx.Response) -> list:
         """Processes Search. Returns list"""
         ...
-    
+
     def movie(self, items: list) -> dict:
         """When a movie is selected, this will process it. Returns dict"""
         ...
-    
+
     def tv(self, items: list) -> dict:
         """When a show is selected, this will process it. Returns dict"""
         ...
-    
