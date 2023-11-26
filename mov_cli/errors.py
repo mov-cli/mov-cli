4989518eee2e4139eb112df4c2c2c04af8c0bb9c
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-11-26 22:52:03 +0000

    docs: updated exception docstring

--- mov_cli/errors.py
+++ mov_cli/errors.py
@@ -11,8 +11,8 @@ from .logger import mov_cli_logger
 __all__ = ("MovCliException",)
 
 class MovCliException(Exception):
-    """Raises whenever there's a known error in goldy bot."""
-    def __init__(self, message:str, logger: logging.Logger = None):
+    """Raises whenever there's a known error in mov-cli."""
+    def __init__(self, message: str, logger: logging.Logger = None):
         message = Colours.RED.apply_to_string(message)
 
         if logger is None:
