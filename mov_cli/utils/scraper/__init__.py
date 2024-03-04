160c1385c97c01a58e24b7be0b069641b9e22cbb
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-03-04 00:55:50 +0000

    feat: remove imdb code and subtitles as we won't need it anymore

--- mov_cli/utils/scraper/__init__.py
+++ mov_cli/utils/scraper/__init__.py
@@ -1,8 +1,4 @@
 """Useful utils for mov-cli scrapers."""
 
-from .imdb import *
-from .subtitles import *
 from .fuzzy import *
 from .the_movie_db import *
-
-# TODO: I think I'll be removing scraper utils and merging it with utils sub module.
