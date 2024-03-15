68ec8825d384895e5afef3d2f31c5ecff817e57b
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-03-15 20:31:39 +0000

    fix, feat: url parameters should be encoded and episode selector can be used here.

--- mov_cli/subtitles.py
+++ mov_cli/subtitles.py
@@ -1,29 +1,53 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .utils.episode_selector import EpisodeSelector
+
 import requests
 
+__all__ = (
+    "Subtitles",
+)
 
 class Subtitles:
     """
     pull subtitles from api.opensubtitles.com and return
     a link to the .srt file which can be passed into vlc or mpv
     """
-
-    def __init__(self, key, language="en"):
+    def __init__(self, key: str, language: str = "en"):
         self.language = language
         self.key = key
         self.base_url = "https://api.opensubtitles.com/api/v1"
         self.get_headers = {
-            "User-Agent": "movcli",
+            "User-Agent": "mov-cli",
             "Api-Key": f"{key}",
         }
 
-    def get_tv_subs(self, name, episode_number, season_number) -> str:
-        url_params = f"/subtitles/?query={name}&languages={self.language}&episode_number={episode_number}&season_number={season_number}"
-        response = requests.get(self.base_url + url_params, headers=self.get_headers)
+    def get_tv_subs(self, name: str, episode: EpisodeSelector) -> str:
+        response = requests.get(
+            self.base_url + "/subtitles/", 
+            params = {
+                "query": name,
+                "languages": self.language,
+                "episode_number": episode.episode,
+                "season_number": episode.season
+            },
+            headers = self.get_headers
+        )
+
         return self.__get_link(response.json()["data"][0]['attributes']['files'][0]['file_id'])
 
-    def get_movie_subs(self, name) -> str:
-        url_params = f"/subtitle/?query={name}&languages={self.language}"
-        response = requests.get(self.base_url + url_params, headers=self.get_headers)
+    def get_movie_subs(self, name: str) -> str:
+        response = requests.get(
+            self.base_url + "/subtitles/", 
+            params = {
+                "query": name,
+                "languages": self.language
+            },
+            headers = self.get_headers
+        )
+
         return self.__get_link(response.json()["data"][0]['attributes']['files'][0]['file_id'])
 
     def __get_link(self, file_id) -> str:
@@ -36,7 +60,7 @@ class Subtitles:
 
         payload = {"file_id": f"{file_id}"}
 
-        response = requests.post(self.base_url + "/download", json=payload, headers=headers)
+        response = requests.post(self.base_url + "/download", json = payload, headers = headers)
         return response.json()['link']
 
 
